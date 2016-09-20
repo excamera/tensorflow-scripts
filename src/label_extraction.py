702606821183eb7e7470912c1e56ed724c70e1d4
kvasukib <kvasukib@eng.ucsd.edu>
2016-09-20 12:58:16 -0700

    Fixing Label Extraction for PNG Images

--- src/label_extraction.py
+++ src/label_extraction.py
@@ -9,15 +9,16 @@ import sys
 import uuid
 import logging
 import time
-from random import randint
 import subprocess as sp
-
 import numpy as np
-from six.moves import urllib
 import tensorflow as tf
 import tensorflow.models.image.imagenet.classify_image as ic
 
-FLAGS = tf.app.flags.FLAGS
+from six.moves import urllib
+from random import randint
+from PIL import Image
+
+#FLAGS = tf.app.flags.FLAGS
 
 ###
 #  Runs inference on an image.
@@ -33,6 +34,9 @@ def run_top_k_predictions_on_image(image):
   if not tf.gfile.Exists(image):
     tf.logging.fatal('File does not exist %s', image)
   image_data = tf.gfile.FastGFile(image, 'rb').read()
+
+  # Download the incepion-v3 model
+  ic.maybe_download_and_extract()
 
   # Creates graph from saved GraphDef.
   ic.create_graph()
@@ -52,17 +56,16 @@ def run_top_k_predictions_on_image(image):
     #   encoding of the image.
     # Runs the softmax tensor by feeding the image_data as input to the graph.
     softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
-    predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_array})
+    predictions = sess.run(softmax_tensor, {'DecodeJpeg:0': image_array})
     predictions = np.squeeze(predictions)
 
     # Creates node ID --> English string lookup.
     node_lookup = ic.NodeLookup()
-    top_k = predictions.argsort()[-FLAGS.num_top_predictions:][::-1]
+    top_k = predictions.argsort()[-ic.FLAGS.num_top_predictions:][::-1]
     for node_id in top_k:
       human_string = node_lookup.id_to_string(node_id)
       score = predictions[node_id]
       print('%s (score = %.5f)' % (human_string, score))
-
 
 # Set Logging
 sys.path.append(".")
